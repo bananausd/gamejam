542b772f3fb2c75959a1fed8186e8f962e99813e
bananausd <melaocanal27@gmail.com>
2026-07-22 12:15:10 -0300

    adicionei o movimento

--- player2.lua
+++ player2.lua
@@ -14,9 +14,32 @@ function player2.load()
 end
 
 function player2.update(dt)
+    movePlayer(dt, 100)
     player2.vel.x = player2.vel.x + 10 * dt
     player2.vel.y = player2.vel.y + 5 * dt
     objs.ChangeObjectPos(player2.selfObj, player2.vel.x * dt, player2.vel.y * dt)
 end
 
+function movePlayer(dt, mvel)
+    keydown("left", function (dt)
+        player.velx = player.velx - mvel * dt
+end, 
+    function ()
+        player.velx = 0
+    end)
+     keydown("right", function (dt)
+        player.velx = player.velx - mvel * dt
+end, 
+    function ()
+        player.velx = 0
+    end)
+end
+
+function keydown(key, cond, cond2)
+    if love.keyboard.isDown(key) then
+        cond()
+    else
+        cond2()
+    end
+end
 return player2
